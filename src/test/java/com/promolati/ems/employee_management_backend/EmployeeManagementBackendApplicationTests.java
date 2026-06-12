32f6df7a586b661e1307aa950b36a6d0981f0ad1
pramod <pramod@sgebiz.com>
2026-06-12 21:30:14 +0800

    Initial setup of Employee Management System backend

---
+++ src/test/java/com/promolati/ems/employee_management_backend/EmployeeManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promolati.ems.employee_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
